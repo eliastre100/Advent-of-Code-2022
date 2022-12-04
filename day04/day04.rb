58ba4aaca1bee1f45dc7b7a1fa8040ca17f25669
Anaïs FORET <anais@foret.dev>
2022-12-04 10:10:34 +0100

    Add day04 part 2

--- day04/day04.rb
+++ day04/day04.rb
@@ -2,15 +2,15 @@ require 'set'
 
 content = File.read(ARGV[0])
 
-base =  content.split("\n")
-            .map { |group|
-              group.split(",")
-                   .map { |elf|
-                     elf.split("-")
-                        .map(&:to_i)
-                        .inject { |a, b| Set.new(a..b) }
-                   }
-            }
+base = content.split("\n")
+              .map { |group|
+                group.split(",")
+                     .map { |elf|
+                       elf.split("-")
+                          .map(&:to_i)
+                          .inject { |a, b| Set.new(a..b) }
+                     }
+              }
 
 puts base.select { |a, b| a.subset?(b) || b.subset?(a) }.count
 puts base.reject { |a, b| a.intersection(b).empty? }.count
