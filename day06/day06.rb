79a9e453ac40dd94b07e860c077c15001b671fa7
Anaïs FORET <anais@foret.dev>
2022-12-06 08:29:12 +0100

    Refacto Day06

--- day06/day06.rb
+++ day06/day06.rb
@@ -2,6 +2,6 @@ content = File.read(ARGV[0]).chars
 
 SEARCH_SIZE = 14
 
-puts(content.map.with_index do |_, index|
-  { tokens: content[index..(index + SEARCH_SIZE - 1)], index: index }
-end.detect { |group| group[:tokens].uniq.size == SEARCH_SIZE }[:index] + SEARCH_SIZE)
+puts((0..(content.size)).detect do |index|
+  content[index..(index + SEARCH_SIZE - 1)].uniq.size == SEARCH_SIZE
+end + SEARCH_SIZE)
