afd40298600bfbe29a5a4c6a04107ef4d880280c
Anaïs FORET <anais@foret.dev>
2022-12-08 10:31:56 +0100

    Add day 08 part 2

--- day08/day08.rb
+++ day08/day08.rb
@@ -1,5 +1,4 @@
 map = File.read(ARGV[0]).split("\n").map { |row| row.split("").map(&:to_i) }
-height_map = Array.new(map.size) { Array.new(map.first.size, 0) }
 
 def dump_map(map)
   map.each do |row|
@@ -26,8 +25,24 @@ def neighbours_value(x, y, map)
   [left, right, top, down].min
 end
 
-puts "map"
-dump_map(map)
+def view_distance(x, y, map, height, view_vector, score = 0)
+  next_pos = { x: x + view_vector[:x], y: y + view_vector[:y] }
+  next_pos_value = position_value(next_pos[:x], next_pos[:y], map)
+  return score if next_pos_value == -1
+  score += 1
+  return score if height <= next_pos_value
+  view_distance(next_pos[:x], next_pos[:y], map, height, view_vector, score)
+end
+
+def scenic_score(x, y, map)
+  height = position_value(x, y, map)
+  left = view_distance(x, y, map, height, { x: -1, y: 0 })
+  right = view_distance(x, y, map, height, { x: 1, y: 0 })
+  top = view_distance(x, y, map, height, { x: 0, y: -1 })
+  down = view_distance(x, y, map, height, { x: 0, y: 1 })
+
+  left * right * top * down
+end
 
 height_map = map.map.with_index do |row, y|
   row.map.with_index do |_, x|
@@ -35,9 +50,12 @@ height_map = map.map.with_index do |row, y|
   end
 end
 
-puts "\nheight map"
-dump_map(height_map)
-
 pp(map.map.with_index do |row, y|
   row.select.with_index { |col, x| col > height_map[y][x] }
 end.flatten.compact.size)
+
+pp((0..(map.size - 1)).map do |y|
+  (0..(map.first.size - 1)).map do |x|
+    scenic_score(x, y, map)
+  end
+end.flatten.max)
